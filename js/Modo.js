1e758306a65a021810e37868dd1ef80923d2ca98
Daniel Silva <aljukillo210@gmail.com>
2024-12-04 06:50:45 -0500

    Update Modo.js

--- js/Modo.js
+++ js/Modo.js
@@ -1,11 +1,14 @@
-
 const toggleButton = document.getElementById('toggle-theme');
 const body = document.body;
-
+const themeIcon = toggleButton.querySelector('img'); 
 
 toggleButton.addEventListener('click', () => {
-
   body.classList.toggle('light-mode');
-});
 
-  
+  if (body.classList.contains('light-mode')) {
+    themeIcon.src = 'img/lun.png';
+  } else {
+    // Si el tema es oscuro, mostramos el sol
+    themeIcon.src = 'img/Sol.png'; // Cambia la imagen a Sol
+  }
+});
